c2850258d10977b28cff98eb9dc7e8e44c1bae87
clarencesea <clarence.sea01@gmail.com>
2022-01-08 11:15:17 +0800

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf build
-node_modules/.bin/babel --minified --source-maps-inline --no-comments src -d build
+node_modules/.bin/babel --minified --source-maps inline --no-comments src -d build
 mkdir -p build/components/ContractSchema/spec
 cp -r src/components/ContractSchema/spec build/components/ContractSchema/spec
 cp scripts/visionbox.js build/.
